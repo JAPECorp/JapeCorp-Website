70dc2b958df5cfa2aa7151fe41234d162ea4a2ee
Eugene <debraheug@gmail.com>
2022-06-06 12:55:46 +0000

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import Clients from '../components/Clients/Clients'
+import Footer from '../components/Footer/footer'
 import Hero from '../components/Hero/hero'
 import Meta from '../components/meta/meta'
 
@@ -6,6 +8,9 @@ export default function Home() {
     <>
       <Meta title="JAPECorp" />
       <Hero id="home" />
+
+      <Clients />
+      <Footer />
 
     </>
   )
