1b34c39de73c04dd6c0060d890967c4c84e88ae0
Eugene <debraheug@gmail.com>
2022-06-06 12:44:05 +0000

    .

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import { BrowserRouter } from 'react-router-dom';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
+import Hero from '../components/Hero/hero'
 import Meta from '../components/meta/meta'
 
 export default function Home() {
   return (
     <>
       <Meta title="JAPECorp" />
-
+      <Hero id="home" />
 
     </>
   )
